591ae57e8655169cba326be996a130e90598d550
rafael ubuntu <ryikeda@outlook.com>
2020-11-09 01:40:15 +0900

    add styles to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,108 @@
 import React from "react";
+import Link from "next/link";
+import { Grid, Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Code as CodeIcon,
+  Brightness5Outlined as SunIcon,
+  NightsStayOutlined as MoonIcon,
+} from "@material-ui/icons";
 
-const Navbar = () => {
-  return <div>This is the Navbar</div>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(1),
+    marginLeft: theme.spacing(3),
+  },
+  navLink: {
+    cursor: "pointer",
+    padding: theme.spacing(0.4),
+    borderRadius: theme.spacing(0.5),
+    "&:hover": {
+      backgroundColor: theme.palette.text.primary,
+      color: theme.palette.background.default,
+    },
+  },
+  modeSwitch: {
+    cursor: "pointer",
+  },
+}));
+
+const Navbar = ({ mode, setMode }) => {
+  const classes = useStyles();
+
+  const menu = [
+    {
+      path: "/",
+      name: "0.START",
+    },
+    {
+      path: "/about",
+      name: "1.ABOUT",
+    },
+    {
+      path: "/blog",
+      name: "2.BLOG",
+    },
+    {
+      path: "/projects",
+      name: "3.PROJECTS",
+    },
+  ];
+
+  const switchMode = () => {
+    const setTo = mode === "dark" ? "light" : "dark";
+    setMode(setTo);
+  };
+
+  return (
+    <Box className={classes.root}>
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+      >
+        <Grid
+          item
+          container
+          xs={10}
+          direction="row"
+          justify="flex-start"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item>
+            <Link href="/" as="/">
+              <Typography variant="body2" color="textPrimary">
+                <CodeIcon />
+              </Typography>
+            </Link>
+          </Grid>
+          {menu.map(({ path, name }) => (
+            <Grid item key={name}>
+              <Link href={path} as={path}>
+                <Typography
+                  variant="body2"
+                  color="textPrimary"
+                  className={classes.navLink}
+                >
+                  {name}
+                </Typography>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+        <Grid item xs={2}>
+          <Box className="NavbarModeSwitchBtn" onClick={() => switchMode()}>
+            <Typography color="textPrimary" className={classes.modeSwitch}>
+              {mode === "dark" ? <SunIcon /> : <MoonIcon />}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Navbar;
