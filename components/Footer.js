c9227c226c84c9e145a113fd26453f09a98e6bbc
rafael ubuntu <ryikeda@outlook.com>
2020-11-09 01:42:02 +0900

    create sample footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 const Footer = () => {
-  return <div>This is the footer</div>;
+  return (
+    <div>
+      <Typography color="textPrimary">This is the footer</Typography>
+    </div>
+  );
 };
 
 export default Footer;
