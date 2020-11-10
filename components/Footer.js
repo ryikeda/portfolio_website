890ccda3d543c3adb821d4a12c4a1b88b36b06d7
rafael ubuntu <ryikeda@outlook.com>
2020-11-10 17:31:21 +0900

    add Footer style

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box, Typography, Link } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import SocialLinks from "../components/SocialLinks";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: theme.spacing(6),
+    marginTop: theme.spacing(5),
     textAlign: "center",
     padding: theme.spacing(3),
   },
@@ -14,11 +15,14 @@ const Footer = () => {
   const classes = useStyles();
   return (
     <Box className={classes.root}>
-      <Typography variant="body1" color="textPrimary">
-        &copy; {new Date().getFullYear()} Code hosted on
+      <SocialLinks />
+      <Typography component="span" variant="body1" color="textPrimary">
+        &copy; {new Date().getFullYear()} Code hosted on&nbsp;
       </Typography>
       <Link href="https://github.com/ryikeda/" rel="noopener" target="_blank">
-        <Typography variant="body1">Github</Typography>
+        <Typography component="span" variant="body1">
+          Github
+        </Typography>
       </Link>
     </Box>
   );
